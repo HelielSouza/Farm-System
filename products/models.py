dbf3f7d73d418f6b2ac0340cf93c2d8e47492de3
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-11-22 18:45:03 -0300

    Creating new models from db

--- products/models.py
+++ products/models.py
@@ -1,17 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Sensor(models.Model):
+    nome_sensor = models.CharField(max_length=20)
+
+
 class CulturaPlantacao(models.Model):
     nome_produto = models.CharField(max_length=100)
+    fk_sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     gd_maximo_acum = models.IntegerField(default=0)
     gd_minimo_acum = models.IntegerField(default=0)
     temp_basal = models.IntegerField(default=0)
 
 
-class Sensor(models.Model):
-    ...
-
-
 class Temperatura(models.Model):
-    ...
+    fk_sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    temperatura = models.FloatField(default=0.0)
+    data = models.DateField(default=timezone.now)
